04bfe1a2a436905ed87993bd3f3d32a07dbc5e3e
Houssame Ait hsain <hossame@inbox.ru>
2023-09-17 16:36:01 +0100

    100

--- elpira/player_drawer.c
+++ elpira/player_drawer.c
@@ -6,19 +6,17 @@ void player_drawer(t_src *src)
 	int py_len;
 
 	py_len = -1;
-	src->px = src->plx * 50;
-	src->py = src->ply * 50;
+	src->px = (src->plx * 50) + 15;
+	src->py = (src->ply * 50) + 15;
 	while (++py_len < 20)
 	{
 		px_len = -1;
-		src->px = 20;
-		src->py = 20;
 		while (++px_len < 20)
 		{
 			mlx_put_pixel(src->img, src->px, src->py, 0xF8003C);
 			src->px++;
 		}
 		src->py++;
-		src->px = src->plx * 50;
+		src->px = (src->plx * 50) + 15;
 	}
 }
