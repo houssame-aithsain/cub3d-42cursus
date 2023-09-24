10e6540379b4dd054062af5b70f5fe0aa08c91ee
Houssame Ait hsain <hossame@inbox.ru>
2023-09-24 20:44:05 +0100

    norm

--- elpira/ray_casting/ray_casting.c
+++ elpira/ray_casting/ray_casting.c
@@ -6,7 +6,7 @@
 /*   By: hait-hsa <hait-hsa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 19:32:07 by hait-hsa          #+#    #+#             */
-/*   Updated: 2023/09/24 20:39:02 by hait-hsa         ###   ########.fr       */
+/*   Updated: 2023/09/24 20:42:32 by hait-hsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,57 +41,6 @@ void	looking_direction(t_src *src)
 	else
 		src->irfr = false;
 	src->irfl = !src->irfr;
-}
-
-void	horz_ray_casting(t_src *src, int stripid)
-{
-	horz_var_init(src);
-	looking_direction(src);
-	horz_setup(src);
-	while (is_horz_wall_(src))
-	{
-		src->x_to_check = src->next_h_touch_x;
-		src->y_to_check = src->next_h_touch_y;
-		if (src->irfu)
-			src->y_to_check -= 1;
-		if (map_has_wall_at(src->x_to_check, src->y_to_check, src))
-		{
-			src->horz_wall_hit_x = src->next_h_touch_x;
-			src->horz_wall_hit_y = src->next_h_touch_y;
-			src->found_h_wall_hit = 1;
-			break ;
-		}
-		else
-		{
-			src->next_h_touch_x += src->xstep;
-			src->next_h_touch_y += src->ystep;
-		}
-	}
-}
-
-void	vert_ray_casting(t_src *src, int stripid)
-{
-	vert_var_init(src);
-	vert_setup(src);
-	while (is_vert_wall_(src))
-	{
-		src->x_to_check = src->next_v_touch_x;
-		src->y_to_check = src->next_v_touch_y;
-		if (src->irfl)
-			src->x_to_check -= 1;
-		if (map_has_wall_at(src->x_to_check, src->y_to_check, src))
-		{
-			src->vert_wall_hit_x = src->next_v_touch_x;
-			src->vert_wall_hit_y = src->next_v_touch_y;
-			src->found_v_wall_hit = 1;
-			break ;
-		}
-		else
-		{
-			src->next_v_touch_x += src->xstep;
-			src->next_v_touch_y += src->ystep;
-		}
-	}
 }
 
 void	get_distance_x_y(t_src *src, int stripid)
